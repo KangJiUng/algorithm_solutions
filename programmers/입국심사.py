7953a3d45b844bd67fc534ad019f57a5fd287644
KangJiUng <woong236406@gmail.com>
2026-01-20 11:14:12 +0900

    입국심사

---
+++ programmers/입국심사.py
@@ -0,0 +1,19 @@
+def solution(n, times):
+    answer = 0
+    left = 1
+    right = max(times) * n
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        total = 0
+        for time in times:
+            total += mid // time
+
+        if total >= n:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return answer
